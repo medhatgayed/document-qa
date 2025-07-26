8ce35da17294716704c3a36c93477f6c91588735
Medhat Gayed <medhat.gayed@gmail.com>
2025-07-26 05:55:32 +0000

    Remove message about adding API Key

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ from openai import OpenAI
 st.title("📄 Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
 
